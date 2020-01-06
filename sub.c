e64da4c4fb901714c1a8266123156090db27e6cc
Pacho <pacho0522@gmail.com>
2020-01-05 20:50:52 -0500

    Adding sub

--- sub.c
+++ sub.c
@@ -12,7 +12,7 @@ void sub(stack_t **head, unsigned int line_number)
 
 	if (!head || !*head || !((*head)->next))
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
