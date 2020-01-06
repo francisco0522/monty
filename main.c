e3d6fa7c0af93001e79d86d119224183f04c2fa2
Pacho <pacho0522@gmail.com>
2020-01-05 21:42:29 -0500

    Fix task 0

--- main.c
+++ main.c
@@ -7,12 +7,19 @@
  */
 int main(int argc, char *argv[])
 {
+	char *file;
+	int leng;
+
 	if (argc != 2)
 	{
 		dprintf(STDERR_FILENO, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	read_file(argv[1]);
+	file = argv[1];
+	leng = strlen(file);
+	leng--;
+	if (file[leng - 1] == '.' && file[leng] == 'm')
+		read_file(argv[1]);
 
 	return (0);
 }
